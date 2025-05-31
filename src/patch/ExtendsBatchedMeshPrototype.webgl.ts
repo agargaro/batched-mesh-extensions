453bc961f6f1883fbb2e13f0062c82e7f281e73e
agargaro <devgargaro@gmail.com>
2025-06-01 01:01:40 +0200

    Refactor BatchedMesh prototype extension to consolidate common methods in a shared function

--- src/patch/ExtendsBatchedMeshPrototype.webgl.ts
+++ src/patch/ExtendsBatchedMeshPrototype.webgl.ts
@@ -1,33 +1,12 @@
 import { BatchedMesh } from 'three';
-import { computeBVH } from '../core/feature/ComputeBVH';
-import { applyMatrixAtToSphere, getPositionAndMaxScaleOnAxisAt, getPositionAt } from '../core/feature/GetPositionAt';
 import { getUniformAt, initUniformsPerInstance, setUniformAt } from '../core/feature/Uniforms';
-import { checkInstanceIntersection, raycast } from '../core/feature/Raycasting';
-import { addGeometryLOD, getLODIndex } from '../core/feature/LOD';
-import { BVHCulling, frustumCulling, linearCulling, onBeforeRender, updateIndexArray, updateRenderList } from '../core/feature/FrustumCulling';
+import { extendBatchedMeshPrototypeCommon } from './ExtendsBatchedMeshPrototype.common';
 
 /**
  * Enhances the BatchedMesh prototype with additional methods.
  */
 export function extendBatchedMeshPrototype(): void {
-  BatchedMesh.prototype.computeBVH = computeBVH;
-
-  BatchedMesh.prototype.onBeforeRender = onBeforeRender;
-  BatchedMesh.prototype.frustumCulling = frustumCulling;
-  BatchedMesh.prototype.updateIndexArray = updateIndexArray;
-  BatchedMesh.prototype.updateRenderList = updateRenderList;
-  BatchedMesh.prototype.BVHCulling = BVHCulling;
-  BatchedMesh.prototype.linearCulling = linearCulling;
-
-  BatchedMesh.prototype.getPositionAt = getPositionAt;
-  BatchedMesh.prototype.getPositionAndMaxScaleOnAxisAt = getPositionAndMaxScaleOnAxisAt;
-  BatchedMesh.prototype.applyMatrixAtToSphere = applyMatrixAtToSphere;
-
-  BatchedMesh.prototype.addGeometryLOD = addGeometryLOD;
-  BatchedMesh.prototype.getLODIndex = getLODIndex;
-
-  BatchedMesh.prototype.raycast = raycast;
-  BatchedMesh.prototype.checkInstanceIntersection = checkInstanceIntersection;
+  extendBatchedMeshPrototypeCommon();
 
   BatchedMesh.prototype.getUniformAt = getUniformAt;
   BatchedMesh.prototype.setUniformAt = setUniformAt;
--- src/patch/ExtendsBatchedMeshPrototype.webgpu.ts
+++ src/patch/ExtendsBatchedMeshPrototype.webgpu.ts
@@ -1,30 +1,8 @@
-import { BatchedMesh } from 'three';
-import { computeBVH } from '../core/feature/ComputeBVH';
-import { applyMatrixAtToSphere, getPositionAndMaxScaleOnAxisAt, getPositionAt } from '../core/feature/GetPositionAt';
-import { checkInstanceIntersection, raycast } from '../core/feature/Raycasting';
-import { addGeometryLOD, getLODIndex } from '../core/feature/LOD';
-import { BVHCulling, frustumCulling, linearCulling, onBeforeRender, updateIndexArray, updateRenderList } from '../core/feature/FrustumCulling';
+import { extendBatchedMeshPrototypeCommon } from './ExtendsBatchedMeshPrototype.common';
 
 /**
  * Enhances the BatchedMesh prototype with additional methods.
  */
 export function extendBatchedMeshPrototype(): void {
-  BatchedMesh.prototype.computeBVH = computeBVH;
-
-  BatchedMesh.prototype.onBeforeRender = onBeforeRender;
-  BatchedMesh.prototype.frustumCulling = frustumCulling;
-  BatchedMesh.prototype.updateIndexArray = updateIndexArray;
-  BatchedMesh.prototype.updateRenderList = updateRenderList;
-  BatchedMesh.prototype.BVHCulling = BVHCulling;
-  BatchedMesh.prototype.linearCulling = linearCulling;
-
-  BatchedMesh.prototype.getPositionAt = getPositionAt;
-  BatchedMesh.prototype.getPositionAndMaxScaleOnAxisAt = getPositionAndMaxScaleOnAxisAt;
-  BatchedMesh.prototype.applyMatrixAtToSphere = applyMatrixAtToSphere;
-
-  BatchedMesh.prototype.addGeometryLOD = addGeometryLOD;
-  BatchedMesh.prototype.getLODIndex = getLODIndex;
-
-  BatchedMesh.prototype.raycast = raycast;
-  BatchedMesh.prototype.checkInstanceIntersection = checkInstanceIntersection;
+  extendBatchedMeshPrototypeCommon();
 }
